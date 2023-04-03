b62dbb06d3600012c707237f33b19fdf98dff81b
meinnn <102753070+meinnn@users.noreply.github.com>
2023-04-03 15:59:09 +0900

    230403

---
+++ 백준/silver/9095.js
@@ -0,0 +1,21 @@
+const fs = require("fs");
+// const input = fs.readFileSync('/dev/stdin').toString().split('\n').map(Number);
+const input = fs
+  .readFileSync("../input.txt")
+  .toString()
+  .trim()
+  .split("\n")
+  .map((e) => +e);
+const T = input.shift();
+
+const arr = [0, 1, 2, 4];
+
+// 0 < n < 11
+for (let i = 4; i < 11; i++) {
+  arr[i] = arr[i - 1] + arr[i - 2] + arr[i - 3];
+}
+
+for (let i = 0; i < T; i++) {
+  const n = input[i];
+  console.log(arr[n]);
+}
