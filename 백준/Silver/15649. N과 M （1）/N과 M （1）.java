dd953adef4d5a89215a1a59da86b26cae43ffae6
meinnn <102753070+meinnn@users.noreply.github.com>
2024-01-31 14:24:30 +0900

    [Silver III] Title: N과 M (1), Time: 2220 ms, Memory: 63976 KB -BaekjoonHub

---
+++ 백준/Silver/15649. N과 M （1）/N과 M （1）.java
@@ -0,0 +1,45 @@
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
+
+public class Main {
+	
+	static int[] numbers;
+	static boolean[] isSelected;
+	
+	static int N, M;
+	
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine());
+		
+		N = Integer.parseInt(st.nextToken());
+		M = Integer.parseInt(st.nextToken());
+		numbers = new int[M];
+		isSelected = new boolean[N+1];
+		perm(0);
+		
+	}
+	
+	static void perm(int cnt) {
+		if (cnt == M) {
+			for (int n : numbers) {
+				System.out.print(n + " ");
+			}
+			System.out.println();
+			return;
+		}
+		
+		for (int i = 1; i <= N; i++) {
+			if (isSelected[i])
+				continue;
+			
+			numbers[cnt] = i;
+			isSelected[i] = true;
+			perm(cnt+1);
+			isSelected[i] = false;
+		}
+		
+	}
+}
