2059f0ffd1b4da5578cf60202ad2172455a6a943
meinnn <102753070+meinnn@users.noreply.github.com>
2024-02-01 17:44:17 +0900

    [Silver II] Title: DNA 비밀번호, Time: 304 ms, Memory: 20024 KB -BaekjoonHub

--- 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
+++ 백준/Silver/12891. DNA 비밀번호/DNA 비밀번호.java
@@ -1,7 +1,5 @@
 import java.io.BufferedReader;
 import java.io.InputStreamReader;
-import java.util.HashMap;
-import java.util.Map;
 import java.util.StringTokenizer;
 
 public class Main {
@@ -22,29 +20,24 @@ public class Main {
 			minAcgt[i] = Integer.parseInt(st.nextToken());
 		}
 		
-		Map<Character, Integer> map = new HashMap<>();
-		map.put('A', 0);
-		map.put('C', 1);
-		map.put('G', 2);
-		map.put('T', 3);
 		
 		int left = 0;
 		int right = P-1;
-		int[] acgt = new int[4];
+		int[] cnt = new int[26]; // 냅다 알파벳 전부 cnt 배열 만들어버리기
 		
 		for (int i = left; i < right; i++) {
-			acgt[map.get(str.charAt(i))]++;
+			cnt[str.charAt(i)-'A']++;
 		}
 		
 		int answer = 0;
 		while (right < S) {
-			acgt[map.get(str.charAt(right))]++;
+			cnt[str.charAt(right)-'A']++;
 			
-			if (isValid(acgt)) {
+			if (isValid(cnt)) {
 				answer++;
 			}
 			
-			acgt[map.get(str.charAt(left))]--;
+			cnt[str.charAt(left)-'A']--;
 			left++;
 			right++;
 		}
@@ -52,11 +45,11 @@ public class Main {
 		System.out.println(answer);
 	}
 
-	public static boolean isValid(int[] acgt) {
-		for (int i = 0; i < 4; i++) {
-			if (acgt[i] < minAcgt[i])
-				return false;
+	public static boolean isValid(int[] cnt) {
+		if (cnt['A'-'A'] >= minAcgt[0] && cnt['C'-'A'] >= minAcgt[1] &&
+				cnt['G'-'A'] >= minAcgt[2] && cnt['T'-'A'] >= minAcgt[3]) {
+			return true;
 		}
-		return true;
+		return false;
 	}
 }
