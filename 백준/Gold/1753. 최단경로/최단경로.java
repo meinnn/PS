a7c34d6eda8cae98986a9dd3656aa59b7c44fa5e
meinnn <102753070+meinnn@users.noreply.github.com>
2024-02-27 16:17:54 +0900

    [Gold IV] Title: 최단경로, Time: 1160 ms, Memory: 121884 KB -BaekjoonHub

---
+++ 백준/Gold/1753. 최단경로/최단경로.java
@@ -0,0 +1,82 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.PriorityQueue;
+import java.util.StringTokenizer;
+
+public class Main {
+
+	static class Node implements Comparable<Node> {
+		int v, cost;
+
+		public Node(int v, int cost) {
+			super();
+			this.v = v;
+			this.cost = cost;
+		}
+
+		@Override
+		public int compareTo(Node o) {
+			return Integer.compare(this.cost, o.cost);
+		}
+
+	}
+
+	static int V;
+	static List<Node>[] graph;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+
+		V = Integer.parseInt(st.nextToken());
+		int E = Integer.parseInt(st.nextToken());
+
+		int start = Integer.parseInt(br.readLine());
+
+		graph = new ArrayList[V + 1];
+		for (int i = 1; i < V + 1; i++)
+			graph[i] = new ArrayList<>();
+
+		for (int i = 0; i < E; i++) {
+			st = new StringTokenizer(br.readLine(), " ");
+
+			int u = Integer.parseInt(st.nextToken());
+			int v = Integer.parseInt(st.nextToken());
+			int w = Integer.parseInt(st.nextToken());
+
+			graph[u].add(new Node(v, w));
+		}
+
+		dijkstra(start);
+	}
+
+	private static void dijkstra(int start) {
+		int[] dist = new int[V + 1];
+		Arrays.fill(dist, Integer.MAX_VALUE);
+
+		PriorityQueue<Node> pq = new PriorityQueue<>();
+		pq.add(new Node(start, 0));
+		dist[start] = 0;
+
+		while (!pq.isEmpty()) {
+			Node cur = pq.poll();
+
+			if (dist[cur.v] < cur.cost)
+				continue;
+
+			for (Node n : graph[cur.v]) {
+				if (dist[n.v] > cur.cost + n.cost) {
+					dist[n.v] = cur.cost + n.cost;
+					pq.add(new Node(n.v, dist[n.v]));
+				}
+			}
+		}
+
+		for (int i = 1; i < V + 1; i++) {
+			System.out.println(dist[i] == Integer.MAX_VALUE ? "INF" : dist[i]);
+		}
+	}
+}
