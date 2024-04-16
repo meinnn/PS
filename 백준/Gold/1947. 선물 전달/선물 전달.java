b414e8ec552b89333d53ec98b1933e7fe9c35b2d
meinnn <102753070+meinnn@users.noreply.github.com>
2024-04-16 10:35:14 +0900

    [Gold III] Title: 선물 전달, Time: 96 ms, Memory: 19356 KB -BaekjoonHub

---
+++ 백준/Gold/1947. 선물 전달/선물 전달.java
@@ -0,0 +1,19 @@
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
+public class Main {
+
+	static final int MOD = 1_000_000_000;
+
+	public static void main(String[] args) throws Exception {
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		int N = Integer.parseInt(br.readLine());
+
+		long[] dp = new long[N + 2];
+		dp[2] = 1;
+		for (int i = 3; i <= N; i++) {
+			dp[i] = (i - 1) * (dp[i - 1] + dp[i - 2]) % MOD;
+		}
+		System.out.println(dp[N]);
+	}
+}
