a0599f2c71a494bfe97f596bd72c2188ce143bdf
meinnn <102753070+meinnn@users.noreply.github.com>
2025-04-16 20:12:19 +0900

    [Silver III] Title: N과 M (2), Time: 92 ms, Memory: 9320 KB -BaekjoonHub

---
+++ 백준/Silver/15650. N과 M （2）/N과 M （2）.js
@@ -0,0 +1,23 @@
+const fs = require('fs');
+const filePath = process.platform === 'linux' ? 0 : 'input.txt';
+const input = fs.readFileSync(filePath).toString().trim();
+
+const [N, M] = input.split(' ').map(Number);
+
+const select = Array(M);
+const result = [];
+
+function comb(start, cnt) {
+  if (cnt === M) {
+    result.push(select.join(' '));
+    return;
+  }
+
+  for (let i = start; i <= N; i++) {
+    select[cnt] = i;
+    comb(i + 1, cnt + 1);
+  }
+}
+
+comb(1, 0);
+console.log(result.join('\n'));
