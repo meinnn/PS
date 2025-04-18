c0b9ff63348a8b724eb79bb3f9228262cfe1b65e
meinnn <102753070+meinnn@users.noreply.github.com>
2025-04-18 20:31:19 +0900

    [Silver III] Title: 주유소, Time: 276 ms, Memory: 44268 KB, Score: 100 point -BaekjoonHub

--- 백준/Silver/13305. 주유소/주유소.js
+++ 백준/Silver/13305. 주유소/주유소.js
@@ -3,15 +3,15 @@ const filePath = process.platform === 'linux' ? 0 : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const N = Number(input[0]);
-const road = input[1].split(' ').map(Number);
-const price = input[2].split(' ').map(Number);
+const road = input[1].split(' ').map(BigInt);
+const price = input[2].split(' ').map(BigInt);
 
-let cost = 0;
-let minPrice = Infinity;
+let cost = 0n;
+let minPrice = price[0];
 
 for (let i = 0; i < N - 1; i++) {
-  minPrice = Math.min(minPrice, price[i]);
+  if (price[i] < minPrice) minPrice = price[i];
   cost += minPrice * road[i];
 }
 
-console.log(cost);
+console.log(cost.toString());
