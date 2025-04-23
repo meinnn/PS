b14c3bdf85c1831654800dc01ef1cfd0b0d7d0ec
meinnn <102753070+meinnn@users.noreply.github.com>
2025-04-24 00:36:42 +0900

    [Silver IV] Title: 수 찾기, Time: 256 ms, Memory: 50304 KB -BaekjoonHub

--- 백준/Silver/1920. 수 찾기/수 찾기.js
+++ 백준/Silver/1920. 수 찾기/수 찾기.js
@@ -3,32 +3,14 @@ const filePath = process.platform === 'linux' ? 0 : 'input.txt';
 const input = fs.readFileSync(filePath).toString().trim().split('\n');
 
 const N = Number(input[0]);
-const A = input[1].split(' ').map(Number);
+const A = new Set(input[1].split(' ').map(Number));
 const M = Number(input[2]);
 const targets = input[3].split(' ').map(Number);
 
 const result = [];
-A.sort((a, b) => a - b);
 
 for (const target of targets) {
-  let left = 0;
-  let right = N - 1;
-  let isExist = 0;
-
-  while (left <= right) {
-    let mid = Math.floor((left + right) / 2);
-
-    if (A[mid] === target) {
-      isExist = 1;
-      break;
-    } else if (A[mid] < target) {
-      left = mid + 1;
-    } else {
-      right = mid - 1;
-    }
-  }
-
-  result.push(isExist);
+  result.push(A.has(target) ? 1 : 0);
 }
 
 console.log(result.join('\n'));
