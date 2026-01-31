eb0517fa84bfb952dba3929a4a7a3f986cad8b90
meinnn <102753070+meinnn@users.noreply.github.com>
2026-01-31 23:58:50 +0900

    [Silver IV] Title: 상근이의 여행, Time: 112 ms, Memory: 20172 KB -BaekjoonHub

--- 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
+++ 백준/Silver/9372. 상근이의 여행/상근이의 여행.js
@@ -8,31 +8,9 @@ const result = [];
 
 for (let t = 0; t < T; t++) {
   const [N, M] = input[line++].split(' ').map(Number);
-  const graph = Array.from({ length: N + 1 }, () => []);
 
-  for (let i = 0; i < M; i++) {
-    const [a, b] = input[line++].split(' ').map(Number);
-    graph[a].push(b);
-    graph[b].push(a);
-  }
-
-  const visited = Array(N + 1).fill(false);
-  let cnt = 0;
-
-  const dfs = (idx) => {
-    visited[idx] = true;
-
-    for (const node of graph[idx]) {
-      if (!visited[node]) {
-        dfs(node);
-        cnt++;
-      }
-    }
-  };
-
-  dfs(1);
-
-  result.push(cnt);
+  line += M;
+  result.push(N - 1);
 }
 
 console.log(result.join('\n'));
