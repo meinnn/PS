cc56de7c2f9980d17a6c834c55e119240a7f475b
meinnn <102753070+meinnn@users.noreply.github.com>
2026-03-28 00:59:56 +0900

    [Gold V] Title: 축구, Time: 92 ms, Memory: 9608 KB -BaekjoonHub

--- 백준/Gold/1344. 축구/축구.js
+++ 백준/Gold/1344. 축구/축구.js
@@ -5,23 +5,15 @@ const input = fs.readFileSync(filePath).toString().trim().split('\n');
 const [pA, pB] = input.map((e) => e / 100);
 
 const TERM = 18;
-const dpA = Array.from({ length: TERM + 1 }, () => Array(TERM + 1).fill(0)); // dp[기간][골]
-const dpB = Array.from({ length: TERM + 1 }, () => Array(TERM + 1).fill(0));
-
-dpA[0][0] = 1.0;
-dpB[0][0] = 1.0;
+const comb = Array.from({ length: TERM + 1 }, () => Array(TERM + 1).fill(0));
 
 for (let i = 1; i <= TERM; i++) {
-  for (let j = 0; j <= TERM; j++) {
-    // 골 넣음
-    if (j > 0) {
-      dpA[i][j] += dpA[i - 1][j - 1] * pA;
-      dpB[i][j] += dpB[i - 1][j - 1] * pB;
+  for (let j = 0; j <= i; j++) {
+    if (j === 0 || j === i) {
+      comb[i][j] = comb[i][i] = 1;
+    } else {
+      comb[i][j] = comb[i][i - j] = comb[i - 1][j - 1] + comb[i - 1][j];
     }
-
-    // 못 넣음
-    dpA[i][j] += dpA[i - 1][j] * (1 - pA);
-    dpB[i][j] += dpB[i - 1][j] * (1 - pB);
   }
 }
 
@@ -29,9 +21,9 @@ const primes = [2, 3, 5, 7, 11, 13, 17];
 let probA = 0;
 let probB = 0;
 
-for (let score of primes) {
-  probA += dpA[TERM][score];
-  probB += dpB[TERM][score];
+for (const time of primes) {
+  probA += comb[TERM][time] * Math.pow(pA, time) * Math.pow(1 - pA, TERM - time);
+  probB += comb[TERM][time] * Math.pow(pB, time) * Math.pow(1 - pB, TERM - time);
 }
 
 const result = 1 - (1 - probA) * (1 - probB);
